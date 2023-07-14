5523b9aeab0288f4b9804302bfa3778928847a31
hln5 <harrynguyen019@gmail.com>
2023-07-13 18:15:12 -0700

    create 2 processes to make api calls (can't make too many or else too many requests are made and the api puts a soft lock on us)

--- 01-load-data.py
+++ 01-load-data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import multiprocessing
+import multiprocessing as mp
 from nba_api.stats.static import players
 from nba_api.stats.endpoints import playercareerstats
 
@@ -10,49 +10,52 @@ COLS = ['PLAYER_NAME', 'PLAYER_ID', 'SEASON_ID', 'LEAGUE_ID', 'TEAM_ID', 'TEAM_A
         'GS', 'MIN', 'FGM', 'FGA', 'FG_PCT', 'FG3M', 'FG3A', 'FG3_PCT', 'FTM', 'FTA', 'FT_PCT', 'OREB', 'DREB',
         'REB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS']
 CUTOFF = 100  # cutoff for number of players to load
+NUM_PLAYERS = 100  # 4815
 
 
 def get_players() -> pd.DataFrame:
     nba_players = players.get_players()
-    nba_players = pd.DataFrame(nba_players)
-    return nba_players[['id', 'full_name']]
+    return pd.DataFrame(nba_players)
 
 
-def get_player_stats(player: pd.Series) -> pd.DataFrame:
-    stats = playercareerstats.PlayerCareerStats(player_id=player['id'])
+def make_api_call(player: pd.Series) -> pd.DataFrame:
+    stats = playercareerstats.PlayerCareerStats(player_id=player['id'], timeout=120)
     stats = stats.get_data_frames()[0]
     stats['PLAYER_NAME'] = player['full_name']
     return stats
 
 
-def get_stats_process(player_data: pd.DataFrame) -> pd.DataFrame:
+def process_stat_batch(player_data: pd.DataFrame, shared_queue):
     players_loaded = 0
     stats = pd.DataFrame(columns=COLS)
 
     for _, player in player_data.iterrows():
-        player_stats = get_player_stats(player)
+        player_stats = make_api_call(player)
         stats = pd.concat([stats, player_stats], ignore_index=True)
         players_loaded += 1
         print(players_loaded)
 
-    return stats
+    shared_queue.put(stats)
+
 
 def main():
     player_data = get_players()
+    print(len(player_data))
 
-    # this will take some time...
-    players_loaded = 0
-    stats = pd.DataFrame(columns=COLS)
-    for _, player in player_data.iterrows():
-        player_stats = get_player_stats(player)
-        stats = pd.concat([stats, player_stats], ignore_index=True)
+    p1 = mp.Process(target=process_stat_batch, args=(player_data[NUM_PLAYERS/2:], q))
+    p2 = mp.Process(target=process_stat_batch, args=(player_data[:NUM_PLAYERS/2], q))
+    p1.start()
+    p2.start()
 
-        players_loaded += 1
-        print(players_loaded)
-        # if players_loaded >= CUTOFF:
-        #     break
-    stats.to_csv(f'{OUT_DIR}/stats.csv')
+    print(q.get())
+    print(q.get())
+
+    p1.join()
+    p2.join()
+
+    # stats.to_csv(f'{OUT_DIR}/stats.csv')
 
 
 if __name__ == '__main__':
+    q = mp.Queue()
     main()
