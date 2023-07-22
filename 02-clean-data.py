18c24bda0a30e351bc339f29a6323c82e3bb51cc
hln5 <harrynguyen019@gmail.com>
2023-07-22 11:01:02 -0700

    add step in pipeline to extract relevant features

--- 02-clean-data.py
+++ 02-clean-data.py
@@ -1,13 +1,22 @@
-import pathlib
+import pandas as pd
+import sys
 
 # constants
 IN_DIR = 'data'
 OUT_DIR = 'cleaned_data'
 
+PLAYER_INFO = ['PLAYER_ID', 'PLAYER_NAME']
+TEAM_STATS = ['TEAM_ABBREVIATION', 'W']
+BASIC_PLAYER_STATS = ['GP', 'PTS', 'REB', 'AST', 'STL', 'BLK', 'TOV', 'PLUS_MINUS']
+
 
 def main():
-    input_dir = pathlib.Path(IN_DIR)
-    output_dir = pathlib.Path(OUT_DIR)
+    seasons = pd.read_csv(f'{IN_DIR}/{sys.argv[1]}')
+
+    # extract relevant features
+    cleaned_seasons = seasons[PLAYER_INFO + TEAM_STATS + BASIC_PLAYER_STATS]
+
+    cleaned_seasons.to_csv(f'{OUT_DIR}/cleaned_pre_allstar_seasons.csv')
 
 
 if __name__ == '__main__':
