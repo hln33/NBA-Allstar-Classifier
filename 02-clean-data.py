75f186568f8ce9628d2db83b9ef8bb2afb9df150
hln5 <harrynguyen019@gmail.com>
2023-07-24 15:24:48 -0700

    clean game logs to have only games before allstar break and remove dupes

--- 02-clean-data.py
+++ 02-clean-data.py
@@ -5,14 +5,21 @@ import sys
 IN_DIR = 'raw_data'
 OUT_DIR = 'cleaned_data'
 
-PLAYER_INFO = ['PLAYER_ID', 'PLAYER_NAME']
-TEAM_STATS = ['TEAM_ABBREVIATION', 'W']
-BASIC_PLAYER_STATS = ['GP', 'PTS', 'REB', 'AST', 'STL', 'BLK', 'TOV', 'PLUS_MINUS']
+
+def clean_game_logs(games: pd.DataFrame) -> pd.DataFrame:
+    games = games.drop_duplicates(subset='GAME_ID')
+    pre_allstar_regex = r'^(199[6-9]|200\d|201[0-9]|202[0-2])-(10|11|12|0[1-2])-.+'
+    games = games[games['GAME_DATE'].str.contains(pre_allstar_regex, regex=True)]
+    return games
 
 
 def main():
-    # do any cleaning that is required...
-    pass
+    games = pd.read_csv(f'{IN_DIR}/game_logs.csv')
+
+    games = clean_game_logs(games)
+    print(games)
+
+    games.to_csv(f'{OUT_DIR}/cleaned_game_logs.csv')
 
 
 if __name__ == '__main__':
