a212443d3cc3017933b79bed5eb1629e6d979b5a
hln5 <harrynguyen019@gmail.com>
2023-07-25 10:15:54 -0700

    edit box score step to use results we have already loaded

--- 03-get-advanced-box-scores.py
+++ 03-get-advanced-box-scores.py
@@ -7,6 +7,13 @@ from nba_api.stats.endpoints import boxscoreadvancedv2
 # constants
 HARD_COOLDOWN = 150
 CACHE_THRESHOLD = 100
+RELOAD_ALL_BOX_SCORES = False
+
+
+# source:https://github.com/swar/nba_api/issues/159
+def cooldown_api():
+    api_cooldown = random.gammavariate(alpha=0.9, beta=0.4)
+    time.sleep(api_cooldown)
 
 
 def get_box_score(game_id):
@@ -17,10 +24,7 @@ def get_box_score(game_id):
         print('request timed out')
         time.sleep(HARD_COOLDOWN)
         data = boxscoreadvancedv2.BoxScoreAdvancedV2(game_id=game_id)
-
-    # source:https://github.com/swar/nba_api/issues/159
-    api_cooldown = random.gammavariate(alpha=0.9, beta=0.4)
-    time.sleep(api_cooldown)
+    cooldown_api()
 
     # sometimes no data is returned from API call, so we must handle it
     try:
@@ -28,7 +32,6 @@ def get_box_score(game_id):
         return team_box_score
     except IndexError as e:
         print('IndexError occurred:', e)
-        print('Skipping game:', game_id)
     return None
 
 
@@ -36,14 +39,22 @@ def get_box_score(game_id):
 # ** this step may take a long time to run (hours) since the api acts as a bottleneck **
 def main():
     game_logs = pd.read_csv('cleaned_data/cleaned_game_logs.csv', dtype={'GAME_ID': str})
+    already_loaded_box_scores = pd.read_csv('raw_data/advanced_box_scores.csv', dtype={'GAME_ID': str})
 
     counter = 0
-    box_scores = pd.DataFrame()
+    box_scores = pd.DataFrame() if RELOAD_ALL_BOX_SCORES else already_loaded_box_scores
     game_ids = game_logs['GAME_ID']
     for _, game_id in game_ids.items():
         print(game_id)
+
+        already_loaded = game_id in already_loaded_box_scores['GAME_ID'].values
+        if not RELOAD_ALL_BOX_SCORES and already_loaded:
+            print("box score already loaded")
+            continue
+
         team_box_score = get_box_score(game_id)
         if team_box_score is None:
+            print("api returned no data:", game_id)
             continue
         box_scores = pd.concat([box_scores, team_box_score], ignore_index=True)
 
